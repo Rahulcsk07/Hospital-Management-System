6e8ebf92803266ef5a4637cba5089ca05d0affd0
Rahulcsk07 <rahulk61194@gmail.com>
2025-01-04 21:15:56 +0530

    testing for first time

---
+++ src/HospitalManagementSystem/test.java
@@ -0,0 +1,12 @@
+package HospitalManagementSystem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
